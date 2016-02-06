e7258ab732405bd3eb107d8d234b96f48ad76020
avijaramuk <kumarajiva2015@gmail.com>
2016-02-05 22:21:17 -0500

    better survey sql deploy

--- survey.sql
+++ survey.sql
@@ -1,7 +1,3 @@
-
--- ----------------------------
--- Table structure for survey
--- ----------------------------
 CREATE TABLE `survey` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `customer` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -9,11 +5,8 @@ CREATE TABLE `survey` (
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `active` bit(1) NOT NULL DEFAULT b'1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
--- ----------------------------
--- Table structure for survey_answer
--- ----------------------------
 CREATE TABLE `survey_answer` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `question` bigint(20) NOT NULL,
@@ -21,21 +14,15 @@ CREATE TABLE `survey_answer` (
   PRIMARY KEY (`id`),
   KEY `answer_question` (`question`),
   CONSTRAINT `answer_question` FOREIGN KEY (`question`) REFERENCES `survey_question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
--- ----------------------------
--- Table structure for survey_answer_choosen
--- ----------------------------
 CREATE TABLE `survey_answer_choosen` (
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `answer` bigint(20) NOT NULL,
   `date_choosen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`email`,`answer`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
--- ----------------------------
--- Table structure for survey_question
--- ----------------------------
 CREATE TABLE `survey_question` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `survey` bigint(20) NOT NULL,
@@ -43,4 +30,4 @@ CREATE TABLE `survey_question` (
   PRIMARY KEY (`id`),
   KEY `question_survay` (`survey`),
   CONSTRAINT `question_survay` FOREIGN KEY (`survey`) REFERENCES `survey` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
