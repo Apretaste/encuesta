d2cd315aaf0c5ba9bde2dfe1fa51cae1797bdd0b
Salvi Pascual <salvi.pascual@gmail.com>
2019-09-03 15:49:35 -0400

    remove the referred field

--- scripts.js
+++ scripts.js
@@ -3,175 +3,182 @@
 //
 // ON LOAD FUNCTIONS
 //
+
 $(document).ready(function () {
-  $('select').formSelect();
-}); //
+	$('select').formSelect();
+});
+
+//
 // FUCTIONS FOR THE SERVICE
 //
+
 // formats a date and time
-
 function formatDateTime(dateStr) {
-  var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-  var date = new Date(dateStr);
-  var month = date.getMonth();
-  var day = date.getDate().toString().padStart(2, '0');
-  var hour = date.getHours() < 12 ? date.getHours() : date.getHours() - 12;
-  var minutes = date.getMinutes();
-  if (minutes < 10) minutes = '0' + minutes;
-  var amOrPm = date.getHours() < 12 ? "am" : "pm";
-  return day + ' de ' + months[month] + ' a las ' + hour + ':' + minutes + amOrPm;
-} // formats a date and time
+	var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+	var date = new Date(dateStr);
+	var month = date.getMonth();
+	var day = date.getDate().toString().padStart(2, '0');
+	var hour = date.getHours() < 12 ? date.getHours() : date.getHours() - 12;
+	var minutes = date.getMinutes();
+	if (minutes < 10) minutes = '0' + minutes;
+	var amOrPm = date.getHours() < 12 ? "am" : "pm";
+	return day + ' de ' + months[month] + ' a las ' + hour + ':' + minutes + amOrPm;
+} 
 
-
+// formats a date and time
 function formatDate(dateStr) {
-  var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-  var date = new Date(dateStr);
-  var month = date.getMonth();
-  var day = date.getDate().toString().padStart(2, '0');
-  return day + ' de ' + months[month] + ' del ' + date.getFullYear();
-} // get list of years for the age
+	var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+	var date = new Date(dateStr);
+	var month = date.getMonth();
+	var day = date.getDate().toString().padStart(2, '0');
+	return day + ' de ' + months[month] + ' del ' + date.getFullYear();
+}
 
-
+// get list of years for the age
 function getYears() {
-  var year = new Date().getFullYear();
-  var years = [];
+	var year = new Date().getFullYear();
+	var years = [];
 
-  for (var i = year - 15; i >= year - 90; i--) {
-    years.push(i);
-  }
+	for (var i = year - 15; i >= year - 90; i--) {
+		years.push(i);
+	}
 
-  return years;
-} // submit the profile informacion 
+	return years;
+} 
 
-
+// submit the profile informacion
 function submitProfileData() {
-  // get the array of fields and  
-  var fields = ['gender', 'year_of_birth', 'skin', 'highest_school_level', 'province']; // create the JSON of data
+	// get the array of fields and  
+	var fields = ['gender', 'year_of_birth', 'skin', 'highest_school_level', 'province']; // create the JSON of data
 
-  var data = {};
+	var data = {};
 
-  for (var i = 0; i < fields.length; i++) {
-    var field = fields[i];
-    var value = $('#' + field).val().trim();
-    if (value) data[field] = value;
-  } // don't let you pass without filling all the fields
+	for (var i = 0; i < fields.length; i++) {
+		var field = fields[i];
+		var value = $('#' + field).val().trim();
+		if (value) data[field] = value;
+	}
 
+	// don't let you pass without filling all the fields
+	if (Object.keys(data).length < 5) {
+		M.toast({
+			html: 'Por favor complete todos los campos antes de continuar'
+		});
+		return false;
+	}
 
-  if (Object.keys(data).length < 5) {
-    M.toast({
-      html: 'Por favor complete todos los campos antes de continuar'
-    });
-    return false;
-  } // save information in the backend
+	// save information in the backend
+	apretaste.send({
+		"command": "PERFIL UPDATE",
+		"data": data,
+		"redirect": false,
+		callback: {
+			name: "callbackReloadEncuesta",
+			data: {}
+		}
+	});
+}
 
-
-  apretaste.send({
-    "command": "PERFIL UPDATE",
-    "data": data,
-    "redirect": false,
-    callback: {
-      name: "callbackReloadEncuesta",
-      data: {}
-    }
-  });
-} // submit a survey once completed
-
-
+// submit a survey once completed
 function submitSurvey() {
-  // variable to save the ID of the responses
-  var friend = $('#friend').val();
-  var answers = [];
-  $('.question').each(function () {
-    // check if the item was checked and return the answer ID
-    var item = $(this).find("input[name='" + this.id + "']:checked").val();
-    answers.push(item); // if no checked, scroll to it and clean the responses
+	// variable to save the ID of the responses
+//	var friend = $('#friend').val();
+	var answers = [];
 
-    if (item == undefined) {
-      // display a message
-      M.toast({
-        html: 'Por favor responda todas las preguntas'
-      }); // scroll to the question
+	$('.question').each(function () {
+		// check if the item was checked and return the answer ID
+		var item = $(this).find("input[name='" + this.id + "']:checked").val();
+		answers.push(item); // if no checked, scroll to it and clean the responses
 
-      $("html, body").animate({
-        scrollTop: $(this).offset().top - 100
-      }, 1000); // clean the responses list to stop sending
+		if (item == undefined) {
+			// display a message
+			M.toast({
+				html: 'Por favor responda todas las preguntas'
+			}); // scroll to the question
 
-      answers = [];
-      return false;
-    }
-  });
+			$("html, body").animate({
+				scrollTop: $(this).offset().top - 100
+			}, 1000); // clean the responses list to stop sending
 
-  if (answers.length) {
-    // send information to the backend
-    apretaste.send({
-      command: "ENCUESTA RESPONDER",
-      data: {
-        friend: friend,
-        answers: answers
-      },
-      redirect: false
-    }); // display the DONE message
+			answers = [];
+			return false;
+		}
+	});
 
-    $('#list').hide();
-    $('#btn').hide();
-    $('#msg').show();
-  }
-} //
+	if (answers.length) {
+		// send information to the backend
+		apretaste.send({
+			command: "ENCUESTA RESPONDER",
+			data: {
+//				friend: friend,
+				answers: answers
+			},
+			redirect: false
+		});
+
+		// display the DONE message
+		$('#list').hide();
+		$('#btn').hide();
+		$('#msg').show();
+	}
+} 
+
+//
 // CALLBACKS
 //
-// redirect to the survey page
-
 
 function callbackReloadEncuesta() {
-  apretaste.send({
-    command: "ENCUESTA"
-  });
-} //
+	apretaste.send({
+		command: "ENCUESTA"
+	});
+}
+
+//
 // PROTOTYPES
 //
 
-
 String.prototype.replaceAll = function (search, replacement) {
-  return this.split(search).join(replacement);
+	return this.split(search).join(replacement);
 };
 
 String.prototype.firstUpper = function () {
-  return this.charAt(0).toUpperCase() + this.substr(1).toLowerCase();
+	return this.charAt(0).toUpperCase() + this.substr(1).toLowerCase();
 };
 
+//
 // POLYFILL
+//
 
 if (!String.prototype.padStart) {
-  String.prototype.padStart = function padStart(targetLength,padString) {
-    targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
-    padString = String((typeof padString !== 'undefined' ? padString : ' '));
-    if (this.length > targetLength) {
-      return String(this);
-    }
-    else {
-      targetLength = targetLength-this.length;
-      if (targetLength > padString.length) {
-        padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
-      }
-      return padString.slice(0,targetLength) + String(this);
-    }
-  };
+	String.prototype.padStart = function padStart(targetLength,padString) {
+		targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+		padString = String((typeof padString !== 'undefined' ? padString : ' '));
+		if (this.length > targetLength) {
+			return String(this);
+		}
+		else {
+			targetLength = targetLength-this.length;
+			if (targetLength > padString.length) {
+				padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+			}
+			return padString.slice(0,targetLength) + String(this);
+		}
+	};
 }
 
 if (!String.prototype.padEnd) {
-  String.prototype.padEnd = function padEnd(targetLength,padString) {
-    targetLength = targetLength>>0; //floor if number or convert non-number to 0;
-    padString = String((typeof padString !== 'undefined' ? padString : ' '));
-    if (this.length > targetLength) {
-      return String(this);
-    }
-    else {
-      targetLength = targetLength-this.length;
-      if (targetLength > padString.length) {
-        padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
-      }
-      return String(this) + padString.slice(0,targetLength);
-    }
-  };
+	String.prototype.padEnd = function padEnd(targetLength,padString) {
+		targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+		padString = String((typeof padString !== 'undefined' ? padString : ' '));
+		if (this.length > targetLength) {
+			return String(this);
+		}
+		else {
+			targetLength = targetLength-this.length;
+			if (targetLength > padString.length) {
+				padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+			}
+			return String(this) + padString.slice(0,targetLength);
+		}
+	};
 }
-
--- service.php
+++ service.php
@@ -4,46 +4,41 @@ use Apretaste\Money;
 
 class EncuestaService extends ApretasteService
 {
+	/**
+	 * Main service
+	 *
+	 * @author salvipascual
+	 */
+	public function _main()
+	{
+		// redirect to the list of surveys opened
+		$this->_lista();
+	}
 
-  /**
-   * Main service
-   *
-   * @author salvipascual
-   */
-  public function _main()
-  {
-    // redirect to the list of surveys opened
-    $this->_lista();
-  }
+	/**
+	 * Edit the person's profile
+	 *
+	 * @author salvipascual
+	 */
+	public function _perfil()
+	{
+		// prepare response for the view
+		return $this->response->setTemplate('profile.ejs', ["profile" => $this->request->person]);
+	}
 
-  /**
-   * Edit the person's profile
-   *
-   * @author salvipascual
-   */
-  public function _perfil()
-  {
-    // prepare response for the view
-    $this->response->setTemplate('profile.ejs', ["profile" => $this->request->person]);
-  }
+	/**
+	 * Get the list of surveys opened
+	 *
+	 * @return void
+	 * @author salvipascual
+	 */
+	public function _lista()
+	{
+		// ensure your profile is completed
+		if ($this->isProfileIncomplete()) return $this->_perfil();
 
-  /**
-   * Get the list of surveys opened
-   *
-   * @return void
-   * @author salvipascual
-   */
-  public function _lista()
-  {
-    // ensure your profile is completed
-    if ($this->isProfileIncomplete()) {
-      $this->_perfil();
-
-      return;
-    }
-
-    // subqueries for the opened surveys
-    $sql_survey_datails = "
+		// subqueries for the opened surveys
+		$sql_survey_datails = "
 			SELECT
 				_survey.id AS survey,
 				_survey.title AS survey_title,
@@ -51,12 +46,14 @@ class EncuestaService extends ApretasteService
 				_survey.value as survey_value
 			FROM _survey
 			WHERE _survey.active = 1 AND _survey.deadline >= CURRENT_DATE";
-    $sql_survey_total_questions = "
+
+		$sql_survey_total_questions = "
 			SELECT COUNT(_survey_question.id) AS total
 			FROM _survey_question
 			WHERE _survey_question.survey =  subq.survey
 			GROUP BY _survey_question.survey";
-    $sql_survey_total_choosen = "
+
+		$sql_survey_total_choosen = "
 			SELECT total FROM (
 				SELECT COUNT(_survey_answer_choosen.answer) as total, (
 					SELECT _survey_question.survey
@@ -72,8 +69,8 @@ class EncuestaService extends ApretasteService
 			) AS subq2
 			WHERE survey_id = subq.survey";
 
-    // get list of opened surveys
-    $surveys = q("
+		// get list of opened surveys
+		$surveys = Connection::query("
 			SELECT
 				survey,
 				survey_title as title,
@@ -83,37 +80,33 @@ class EncuestaService extends ApretasteService
 			FROM ($sql_survey_datails) as subq
 			WHERE coalesce(($sql_survey_total_questions),0) > coalesce(($sql_survey_total_choosen),0);");
 
-    // message if there are not opened surveys
-    if (empty($surveys)) {
-      return $this->response->setTemplate('message.ejs', [
-        "header" => "No hay encuestas",
-        "icon"   => "sentiment_very_dissatisfied",
-        "text"   => "Lo siento pero no tenemos ninguna encuesta para usted en este momento. Estamos trabajamos en agregar encuestas a nuestra lista, por favor vuelva a revisar en unos días. Muchas gracias por estar pendiente.",
-        "button" => ["href" => "ENCUESTA TERMINADAS", "caption" => "Ver Terminadas"],
-      ]);
-    }
+		// message if there are not opened surveys
+		if (empty($surveys)) {
+			return $this->response->setTemplate('message.ejs', [
+				"header" => "No hay encuestas",
+				"icon"   => "sentiment_very_dissatisfied",
+				"text"   => "Lo siento pero no tenemos ninguna encuesta para usted en este momento. Estamos trabajamos en agregar encuestas a nuestra lista, por favor vuelva a revisar en unos días. Muchas gracias por estar pendiente.",
+				"button" => ["href" => "ENCUESTA TERMINADAS", "caption" => "Ver Terminadas"],
+			]);
+		}
 
-    // send response to the user
-    $this->response->setTemplate('list.ejs', ['surveys' => $surveys]);
-  }
+		// send response to the user
+		$this->response->setTemplate('list.ejs', ['surveys' => $surveys]);
+	}
 
-  /**
-   * Display a list of previous surveys
-   *
-   * @return void
-   * @author salvipascual
-   */
-  public function _terminadas()
-  {
-    // ensure your profile is completed
-    if ($this->isProfileIncomplete()) {
-      $this->_perfil();
+	/**
+	 * Display a list of previous surveys
+	 *
+	 * @return void
+	 * @author salvipascual
+	 */
+	public function _terminadas()
+	{
+		// ensure your profile is completed
+		if ($this->isProfileIncomplete()) return $this->_perfil();
 
-      return;
-    }
-
-    //get the list of surveys answered
-    $completed = q("
+		//get the list of surveys answered
+		$completed = Connection::query("
 			SELECT person_id, responses, total, C.title, C.value, A.inserted
 			FROM (SELECT person_id, survey, COUNT(survey) as responses, MAX(date_choosen) AS inserted FROM _survey_answer_choosen WHERE person_id='{$this->request->person->id}' GROUP BY survey) A
 			LEFT JOIN (SELECT survey, COUNT(survey) as total FROM _survey_question GROUP BY survey) B
@@ -122,40 +115,34 @@ class EncuestaService extends ApretasteService
 			ON A.survey = C.id
 			WHERE responses = total");
 
-    // message if there are not opened surveys
-    if (empty($completed)) {
-      $this->response->setTemplate('message.ejs', [
-        "header" => "No ha completado encuestas",
-        "icon"   => "sentiment_neutral",
-        "text"   => "Usted aún no ha completado ninguna encuesta. Cuando responda por primera vez se agregará a esta lista.",
-        "button" => ["href" => "ENCUESTA", "caption" => "Ver Encuestas"],
-      ]);
+		// message if there are not opened surveys
+		if (empty($completed)) {
+			return $this->response->setTemplate('message.ejs', [
+				"header" => "No ha completado encuestas",
+				"icon"   => "sentiment_neutral",
+				"text"   => "Usted aún no ha completado ninguna encuesta. Cuando responda por primera vez se agregará a esta lista.",
+				"button" => ["href" => "ENCUESTA", "caption" => "Ver Encuestas"],
+			]);
+		}
 
-      return;
-    }
+		// send response to the user
+		$this->response->setCache(12 * 60 * 60);
+		$this->response->setTemplate('completed.ejs', ['surveys' => $completed]);
+	}
 
-    // send response to the user
-    $this->response->setCache(12 * 60 * 60);
-    $this->response->setTemplate('completed.ejs', ['surveys' => $completed]);
-  }
+	/**
+	 * Display a survey to answer it
+	 *
+	 * @return void
+	 * @author salvipascual
+	 */
+	public function _ver()
+	{
+		// ensure your profile is completed
+		if ($this->isProfileIncomplete()) return $this->_perfil();
 
-  /**
-   * Display a survey to answer it
-   *
-   * @return void
-   * @author salvipascual
-   */
-  public function _ver()
-  {
-    // ensure your profile is completed
-    if ($this->isProfileIncomplete()) {
-      $this->_perfil();
-
-      return;
-    }
-
-    // get the survey details
-    $res = q("
+		// get the survey details
+		$res = Connection::query("
 			SELECT
 				_survey.id AS survey,
 				_survey.title AS survey_title,
@@ -179,170 +166,163 @@ class EncuestaService extends ApretasteService
 			WHERE _survey.id = {$this->request->input->data->id}
 			ORDER BY _survey_question.id, _survey_answer.id");
 
-    // do not process invalid responses
-    if (empty($res) || !isset($res[0])) {
-      return;
-    }
+		// do not process invalid responses
+		if (empty($res) || !isset($res[0])) return;
 
-    // message if the survey was already completed
-    if ($this->isSurveyComplete($res[0]->survey)) {
-      $this->response->setTemplate('message.ejs', [
-        "header" => "¡Chócala! Ya respondió esta encuesta",
-        "icon"   => "pan_tool",
-        "text"   => "Usted ya respondió esta encuesta, y como agradecimiento se le agregaron §{$res[0]->survey_value} a su crédito. Muchas gracias por su participación.",
-        "button" => ["href" => "ENCUESTA", "caption" => "Ver Encuestas"],
-      ]);
+		// message if the survey was already completed
+		if ($this->isSurveyComplete($res[0]->survey)) {
+			return $this->response->setTemplate('message.ejs', [
+				"header" => "¡Chócala! Ya respondió esta encuesta",
+				"icon"   => "pan_tool",
+				"text"   => "Usted ya respondió esta encuesta, y como agradecimiento se le agregaron §{$res[0]->survey_value} a su crédito. Muchas gracias por su participación.",
+				"button" => ["href" => "ENCUESTA", "caption" => "Ver Encuestas"],
+			]);
+		}
 
-      return;
-    }
+		// create a new Survey object
+		$survey = new stdClass();
+		$survey->id = $res[0]->survey;
+		$survey->title = $res[0]->survey_title;
+		$survey->details = $res[0]->survey_details;
+		$survey->value = $res[0]->survey_value;
+		$survey->questions = [];
 
-    // create a new Survey object
-    $survey = new stdClass();
-    $survey->id = $res[0]->survey;
-    $survey->title = $res[0]->survey_title;
-    $survey->details = $res[0]->survey_details;
-    $survey->value = $res[0]->survey_value;
-    $survey->questions = [];
+		// create the list of questions
+		foreach ($res as $r) {
+			// create the question if it does not exist
+			$question = end($survey->questions);
+			if (empty($question) || $question->id != $r->question) {
+				$question = new stdClass();
+				$question->id = $r->question;
+				$question->title = $r->question_title;
+				$question->answers = [];
+				$question->completed = false;
+				$survey->questions[] = $question;
+			}
 
-    // create the list of questions
-    foreach ($res as $r) {
-      // create the question if it does not exist
-      $question = end($survey->questions);
-      if (empty($question) || $question->id != $r->question) {
-        $question = new stdClass();
-        $question->id = $r->question;
-        $question->title = $r->question_title;
-        $question->answers = [];
-        $question->completed = false;
-        $survey->questions[] = $question;
-      }
+			// create the answers for the question
+			$answer = new stdClass();
+			$answer->id = $r->answer;
+			$answer->title = $r->answer_title;
+			$answer->choosen = $r->choosen == '1';
 
-      // create the answers for the question
-      $answer = new stdClass();
-      $answer->id = $r->answer;
-      $answer->title = $r->answer_title;
-      $answer->choosen = $r->choosen == '1';
+			// mark question as completed
+			if ($answer->choosen) {
+				$question->completed = true;
+			}
 
-      // mark question as completed
-      if ($answer->choosen) {
-        $question->completed = true;
-      }
+			// assign the answer to the question
+			$question->answers[] = $answer;
+		}
 
-      // assign the answer to the question
-      $question->answers[] = $answer;
-    }
+		// send response to the view
+		$this->response->setTemplate('survey.ejs', ['survey' => $survey]);
+	}
 
-    // send response to the view
-    $this->response->setTemplate('survey.ejs', ['survey' => $survey]);
-  }
+	/**
+	 * Responds a survey
+	 *
+	 * @author salvipascual
+	 * @return void
+	 * @throws \Exception
+	 */
+	public function _responder()
+	{
+		// do not continue if data is not passed
+		if (empty($this->request->input->data->answers)) return;
 
-    /**
-     * Responds a survey
-     *
-     *
-     * @return void
-     * @throws \Exception
-     * @author salvipascual
-     */
-  public function _responder()
-  {
-    // do not continue if data is not passed
-    if (empty($this->request->input->data->answers)) {
-      return;
-    }
+		// get the question IDs for the answers received
+		$answers = implode(",", $this->request->input->data->answers);
+		$questions = Connection::query("SELECT question FROM _survey_answer WHERE id IN ($answers)");
 
-    // get the question IDs for the answers received
-    $answers = implode(",", $this->request->input->data->answers);
-    $questions = q("SELECT question FROM _survey_answer WHERE id IN ($answers)");
-
-    // get the survey
-    $survey = q("
+		// get the survey
+		$survey = Connection::query("
 			SELECT A.id, A.value, A.title
 			FROM _survey A
 			JOIN _survey_question B
 			ON A.id = B.survey
 			WHERE B.id = {$questions[0]->question}")[0];
 
-    if (!isset($survey->id)) {
-      $this->simpleMessage("Encuesta no encontrada", "Los datos recibidos por ti no concuerdan con las encuestas que tenemos. Por favor, prueba borrar la cache de la aplicacion. Si el problema persiste contacte al soporte tecnico de A!");
-      return;
-    }
+		if (!isset($survey->id)) {
+			$this->simpleMessage("Encuesta no encontrada", "Los datos recibidos por ti no concuerdan con las encuestas que tenemos. Por favor, prueba borrar la cache de la aplicacion. Si el problema persiste contacte al soporte tecnico de A!");
+			return;
+		}
 
-    if ($this->isSurveyComplete($survey->id)) {
-        $this->simpleMessage("Encuesta completada", "La encuesta ha sido completada por usted anteriormente.");
-        return;
-    }
+		if ($this->isSurveyComplete($survey->id)) {
+			$this->simpleMessage("Encuesta completada", "La encuesta ha sido completada por usted anteriormente.");
+			return;
+		}
 
-    // prepare the data to be sent in one large query
-    $values = [];
-    for ($i = 0, $iMax = count($this->request->input->data->answers); $i < $iMax; $i++) {
-      $questionID = $questions[$i]->question;
-      $answerID = $this->request->input->data->answers[$i];
-      $values[] = "('{$this->request->person->id}', '{$this->request->person->email}', {$survey->id}, $questionID, $answerID)";
-    }
-    $values = implode(",", $values);
+		// prepare the data to be sent in one large query
+		$values = [];
+		for ($i = 0, $iMax = count($this->request->input->data->answers); $i < $iMax; $i++) {
+			$questionID = $questions[$i]->question;
+			$answerID = $this->request->input->data->answers[$i];
+			$values[] = "('{$this->request->person->id}', '{$this->request->person->email}', {$survey->id}, $questionID, $answerID)";
+		}
+		$values = implode(",", $values);
 
-    // replace all old answers by the new answers in one query
-    q("
+		// replace all old answers by the new answers in one query
+		Connection::query("
 			START TRANSACTION;
 			DELETE FROM _survey_answer_choosen WHERE person_id = '{$this->request->person->id}' AND survey = '{$survey->id}';
 			INSERT INTO _survey_answer_choosen (person_id, email, survey, question, answer) VALUES $values;
 			COMMIT;");
 
-    // add § for the user if all questions were completed
-    if ($this->isSurveyComplete($survey->id)) {
-        Money::transfer(Money::BANK, $this->request->person->id, $survey->value, "ENCUESTA {$survey->id}", "Ha ganado §{$survey->value} por contestar la encuesta {$survey->title}");
-//		Utils::addCredit($survey->value, "ENCUESTA {$survey->id}" , $this->request->person->id);
-		Connection::query("UPDATE _survey SET answers=answers+1 WHERE id='{$survey->id}'");
-    }
+		// add § for the user if all questions were completed
+		if ($this->isSurveyComplete($survey->id)) {
+			Money::transfer(Money::BANK, $this->request->person->id, $survey->value, "ENCUESTA {$survey->id}", "Ha ganado §{$survey->value} por contestar la encuesta {$survey->title}");
+			Connection::query("UPDATE _survey SET answers=answers+1 WHERE id='{$survey->id}'");
+		}
 
-    // if there is a referred, add it to the table and grant credits
-    if (!empty($this->request->input->data->friend)) {
-      $credit = 0;
-      $friend = Utils::getPerson($this->request->input->data->friend);
-      if ($friend && $friend->id !== $this->request->person->id) {
-		  // add credits to the friend
-		  $credit = 1;
-		  Money::transfer(Money::BANK, $friend->id, $credit, 'ENCUESTA REFERIR', "Ha ganado §$credit por referir a @{$this->request->person->username} a nuestra encuesta. Gracias!");
+/* @NOTE: REFERRED CREDITS CLOSED DOWN FOR NOW 
 
-          // add refer record to the table
-          Connection::query("INSERT INTO _survey_referred (person_id, survey_id, referred, credit) 
-				VALUES ({$this->request->person->id}, {$survey->id}, '{$friend->email}', $credit)");
-      }
+		// if there is a referred, add it to the table and grant credits
+		if (!empty($this->request->input->data->friend)) {
+			$friend = Utils::getPerson($this->request->input->data->friend);
+			if ($friend && $friend->id !== $this->request->person->id) {
+				// amount of referred credits
+				$credit = 1;
 
+				// add credits to the friend
+				Money::transfer(Money::BANK, $friend->id, $credit, 'ENCUESTA REFERIR', "Ha ganado §$credit por referir a @{$this->request->person->username} a nuestra encuesta. Gracias!");
 
-    }
-  }
+				// add refer record to the table
+				Connection::query("INSERT INTO _survey_referred (person_id, survey_id, referred, credit) VALUES ({$this->request->person->id}, {$survey->id}, '{$friend->email}', $credit)");
+			}
+		}
+*/
+	}
 
-  /**
-   * Check if the survey is completed
-   *
-   * @param String $surveyID
-   *
-   * @return Boolean, true if survey is 100% completed
-   * @author salvipascual
-   */
-  private function isSurveyComplete($surveyID)
-  {
-    $res = q("
+	/**
+	 * Check if the survey is completed
+	 *
+	 * @param String $surveyID
+	 *
+	 * @return Boolean, true if survey is 100% completed
+	 * @author salvipascual
+	 */
+	private function isSurveyComplete($surveyID)
+	{
+		$res = Connection::query("
 			SELECT * FROM
 			(SELECT COUNT(survey) as total FROM _survey_question WHERE survey='$surveyID') A,
 			(SELECT COUNT(answer) as answers FROM _survey_answer_choosen WHERE survey='$surveyID' AND person_id='{$this->request->person->id}') B");
 
-    return $res[0]->total === $res[0]->answers;
-  }
+		return $res[0]->total === $res[0]->answers;
+	}
 
-  /**
-   * Check if the profile is completed or not
-   *
-   * @return Boolean, true if profile is incomplete
-   * @author salvipascual
-   */
-  private function isProfileIncomplete()
-  {
-    return $this->request->person->age < 5 || $this->request->person->age > 130
-      || empty($this->request->person->province)
-      || empty($this->request->person->skin)
-      || empty($this->request->person->highest_school_level);
-  }
+	/**
+	 * Check if the profile is completed or not
+	 *
+	 * @return Boolean, true if profile is incomplete
+	 * @author salvipascual
+	 */
+	private function isProfileIncomplete()
+	{
+		return $this->request->person->age < 5 || $this->request->person->age > 130
+			|| empty($this->request->person->province)
+			|| empty($this->request->person->skin)
+			|| empty($this->request->person->highest_school_level);
+	}
 }
